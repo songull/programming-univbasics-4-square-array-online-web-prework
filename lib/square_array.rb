00d3ad6c64a6fadb0f34e67433d4877a863020e2
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-10 03:33:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,5 @@
 def square_array(array)
-  numbers=[10,15,20]
-  square_array(numbers)
-  new_number=[10,30,30]
-  square_array(new_number)
+  while array[square_array]do 
+    puts array[square_array]
+    array += 1
 end
